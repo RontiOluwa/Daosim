47555beb7a900005bb4e806430fc2f3cecc56174
RontiOluwa <olaoluwaojedapo@gmail.com>
2022-04-27 09:14:24 +0100

    feat: added validation for supported chain

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { useEthers } from "@usedapp/core";
 import Nav from "./component/Nav";
 import Transfer from "./page/Transfer";
 import Mint from "./page/Mint";
 import Profile from "./page/Profile";
+import SUPPORTED_TEST_CHAINS from "./chain.json";
 
 function App() {
+  const { chainId, account }: any = useEthers();
+  const isSupportedChain = SUPPORTED_TEST_CHAINS.includes(chainId);
   return (
     <div className="App p-4">
       <BrowserRouter>
         <Nav />
         <div className="max-w-screen-sm m-auto pt-20">
-          <Routes>
-            <Route path="/" element={<Mint />} />
-            <Route path="/transfer" element={<Transfer />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
+          {isSupportedChain ? (
+            <Routes>
+              <Route path="/" element={<Mint />} />
+              <Route path="/transfer" element={<Transfer />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          ) : (
+            <p className="text-center text-2xl">Set network to: Rinkeby</p>
+          )}
         </div>
       </BrowserRouter>
     </div>
--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,15 +1,17 @@
 import { ButtonField } from "../utils/interface";
 import { useEthers } from "@usedapp/core";
 
-function Button({ text, action }: ButtonField) {
+function Button(this: any, { text, action }: ButtonField) {
   const { account } = useEthers();
 
   return (
     <div className="Input text-center">
       <button
         disabled={account ? false : true}
-        className="px-4 py-2 font-semibold text-sm bg-sky-500 text-white rounded-md shadow-sm opacity-100 mt-5 m-auto w-full"
-        onClick={action}
+        className={`${
+          !account && "cursor-not-allowed"
+        } px-4 py-2 font-semibold text-sm bg-sky-500 text-white rounded-md shadow-sm opacity-100 mt-5 m-auto w-full`}
+        onClick={() => action}
       >
         {text}
       </button>
